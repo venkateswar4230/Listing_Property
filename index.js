e38894cb5a1762638803081414b1163ddc3644ba
Utttam <uttamthapa4230@gmail.com>
2026-04-21 02:37:41 +0530

    change PORT

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express=require('express');
 const app=express();
 const mongoose=require('mongoose');
 const Listing= require('./models/listing');
-const port=process.env.PORT || 4000;
+const port=process.env.PORT || 3000;
 require('dotenv').config();
 const path=require('path')
 const methodOverride = require('method-override');
